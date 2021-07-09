b52e180a24358890516f46e462fb5d0a703f5fde
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-07-09 23:00:51 +0900

    初期表示をテスト

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it('renders empty screen at first', () => {
+    render(<App />);
+    const emptyElement = screen.getByText(/TODO List is Empty.../i);
+    expect(emptyElement).toBeInTheDocument();
+  });
 });
