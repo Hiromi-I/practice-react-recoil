2dccbd1d02b79dc0455a4ff5d555cdc35c73ad91
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-07-10 21:53:14 +0900

    テストコードを微修正

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-describe("App", () => {
-  it('renders empty screen at first', () => {
+describe("<App />", () => {
+  it("renders empty screen at first", () => {
     render(<App />);
     const emptyElement = screen.getByText(/TODO List is Empty.../i);
     expect(emptyElement).toBeInTheDocument();
