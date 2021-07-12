df3780e6b716832bce74c143e726d304b0098c3e
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-07-12 23:34:03 +0900

    Todoを完了状態でフィルタリンするために管理用stateを作成

--- src/datas/todo.ts
+++ src/datas/todo.ts
@@ -1,8 +1,30 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 import { TodoType } from "../types/todo";
 
 export const todoListState = atom<Array<TodoType>>({
   key: "TodoListState",
   default: [],
+});
+
+export const todoListFilterState = atom<string>({
+  key: "TodoListFilterState",
+  default: "All",
+});
+
+export const filterdTodoListState = selector<Array<TodoType>>({
+  key: "FilterdTodoListState",
+  get: ({ get }) => {
+    const list = get(todoListState);
+    const filter = get(todoListFilterState);
+
+    switch(filter) {
+      case "Completed":
+        return list.filter(item => item.isComplete === true);
+      case "Uncompleted":
+        return list.filter(item => item.isComplete === false);
+      default:
+        return list;
+    }
+  },
 });
